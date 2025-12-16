919a04ed6f4309e51bafb063c88d702035d81529
Nic <73386479+nicyoong@users.noreply.github.com>
2025-12-16 09:39:19 +0800

    discord bot setup

--- mainbot.py
+++ mainbot.py
@@ -1,7 +1,17 @@
 import json
 import textwrap
 import discord
+import os
 from discord import app_commands
+from dotenv import load_dotenv
 
 import pragmatics
 import prconfig
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
